227fc7b3ee8ecb9dee3279aa3b1ec45efd128d32
Vasco-OD <148139838+Vasco-OD@users.noreply.github.com>
2023-10-21 23:17:56 +0100

    Update indivíduos.py

--- indivíduos.py
+++ indivíduos.py
@@ -5,27 +5,27 @@ import random as random
 
 #new(c,t,n) - cria um indivíduo com a coloração c, nascido no momento t, com o identificador n (devolve o indivíduo criado)
 def new(c,t,n):
-  return[c,t,n]
+    return[c,t,n]
 
 #coef(i) - devolve o coeficiente de adaptação de i
 def coef(i):
-  if color.num_erros(i[0])==0:
-    return graph.dim(color.grafo(i[0]))/color.num_cores(i[0])
-  else:
-    return 1/(color.num_erros(i[0])+1)
+    if color.num_erros(i[0])==0:
+        return graph.dim(color.grafo(i[0]))/color.num_cores(i[0])
+    else:
+        return 1/(color.num_erros(i[0])+1)
 
 #idade(i) - devolve o momento de criação de i
 def idade(i):
-  return i[1]
+    return i[1]
 
 #mutation(i) - altera uma cor na coloração de i (devolve o indivíduo evoluído)
 def mutation(i):
-  v=random.randrange(1,graph.dim(color.grafo(i[0]))+1)
-  n=random.randrange(1,color.num_cores(i[0])+1)
-  i[0]=color.cor(i[0],v,n)
-  return i
+    v=random.randrange(1,graph.dim(color.grafo(i[0]))+1)
+    n=random.randrange(1,color.num_cores(i[0])+1)
+    i[0]=color.cor(i[0],v,n)
+    return i
 
 
 #ident(i) - devolve o identificador do indivíduo 
 def ident(i):
-  return i[2]
+    return i[2]

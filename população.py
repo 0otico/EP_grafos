4b4666fed90f0416c2772936ec7fe5c53d81f9a9
luisanandrade <148136759+luisanandrade@users.noreply.github.com>
2023-10-21 20:07:07 +0100

    Create população.py

---
+++ população.py
@@ -0,0 +1,42 @@
+#População - Lista p de indivíduos
+import individuos as ind
+
+#new() - cria uma população nova (devolve a população criada)
+def new():
+  return []
+
+#emptyQ(p) - devolve True se a população p não tiver indivíduos; devolve False caso contrário
+def emptyQ(p):
+  if p=[]
+    return True
+  else:
+    return False 
+
+#ident(p,i) - devolve o indivíduo da população p com o identificador i
+def ident(p,i):
+  return ind.ident(i) 
+
+#best(p) - devolve o indivíduo de p com melhor coeficiente de adaptação
+def best(p):
+def auxmax(p):
+  ind.coef(i):
+return auxmax(p) 
+
+#worst(p) - devolve o indivíduo de p com pior coeficiente de adaptação 
+def worst(p):
+  def listadecoefs(p,i)
+  return ind.coef(i)
+    def minimo(ind.coef(i)): #função que calcula o minimo de uma lista
+         res=ind.coef(i)[0]
+        for x in ind.coef(i):
+            if x<res:
+                res=x
+        return res
+return minimo
+
+
+#addI(p,i) - adiciona o indivíduo i à população p e devolve p
+def addI(p,i)
+
+
+#kill(p,i) - tira o indivíduo i da lista p e devolve-a 

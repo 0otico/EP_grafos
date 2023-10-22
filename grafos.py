6eed87dac51aa0091078040d9efd5d954f8e0b75
Martaaaaa1997 <148141613+Martaaaaa1997@users.noreply.github.com>
2023-10-22 16:41:33 +0100

    Update grafos.py

--- grafos.py
+++ grafos.py
@@ -9,7 +9,7 @@ def new(n): #cria um grafo com n vértices, e sem arestas
 def addedge(g,x,y): #adiciona a aresta (x,y) ao grafo g
     if not y in g[x-1] and x!=y: 
         #só adiciona a aresta se ela não existir
-        #o grafo não pode ter arestas para si mesmo, senão nenhuma coloração é válida
+        #um dos vértices não pode ter arestas para si mesmo, senão nenhuma coloração é válida
         #em princípio, se y pertence a g[x-1] então x pertence a g[y-1]
         g[x-1]=[n for n in g[x-1] if n<y]+[y]+[n for n in g[x-1] if n>y]
         g[y-1]=[n for n in g[y-1] if n<x]+[x]+[n for n in g[y-1] if n>x]
@@ -33,7 +33,7 @@ def emptyQ(g): #verifica se o gráfico é vazio (não tem arestas)
     return res
 
 def edge(g,x,y): #verifica se a aresta (x,y) existe em g
-    if y in g[x-1] and x in g[y-1] and x!=y: #é redundante, mas better safe than sorry 
+    if y in g[x-1] and x in g[y-1] and x!=y:
         return True
     else:
         return False

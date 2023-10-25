9d8d8ee3c6b01513d7147e95f9d7e6edf3d9cfee
Vasco-OD <148139838+Vasco-OD@users.noreply.github.com>
2023-10-25 15:37:59 +0100

    Update população.py

--- população.py
+++ população.py
@@ -12,13 +12,23 @@ def new():
 #dim(p) - devolve a dimensão da população (número de elementos)
 def dim(p):
     return len(p)
+
+
+#identQ(p,i) - devolve True se o indivíduo com identificador i existir na população p
+def identQ(p,i):
+    k=0
+    found=False
+    while k<len(p) and not found:
+        found = ind.ident(p[k])==i
+        i+=1
+    return found
+
     
 #ident(p,i) - devolve o indivíduo da população p com o identificador i
 def ident(p,i):
+    if identQ(p,i):
     k=0
     found=False
-    if i == 0: # i==0 para eventos globais, sem identificador específico
-        return True
     while k<len(p) and not found:
         if ind.ident(p[k])==i:
             found=True
@@ -26,8 +36,7 @@ def ident(p,i):
             k+=1
     if found: #devolve o indivíduo, se existir
         return p[k]
-    else: #devolve False se não existir indivíduo
-        return False 
+    
 
 #best(p) - devolve o indivíduo de p com melhor coeficiente de adaptação
 def best(p):

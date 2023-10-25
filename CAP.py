12975e0128d6f1bad128eb27ec31422f1f88ab01
Vasco-OD <148139838+Vasco-OD@users.noreply.github.com>
2023-10-25 18:29:34 +0100

    Update CAP.py

--- CAP.py
+++ CAP.py
@@ -1,5 +1,4 @@
 # Uma agenda (CAP) é uma lista c de eventos.
-# O nome da agenda pode mudar com as várias operações (add, delete, delete_id)
 
 import eventos as event
 
@@ -7,7 +6,7 @@ def new(): #cria uma agenda vazia
     return []
 
 def add(e,c): #adiciona o evento e à agenda c
-    #parte do pressuposto que c está ordenada
+    #mantém a agenda ordenada por instante dos eventos
     return [n for n in c if event.time(n)<=event.time(e)]+[e]+[n for n in c if event.time(n)>event.time(e)] 
 
 def nextE(c): #devolve o próximo evento na agenda c

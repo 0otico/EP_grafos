40021b16adca2b2a9a085a5a744da325b9e30e86
Martaaaaa1997 <148141613+Martaaaaa1997@users.noreply.github.com>
2023-10-26 15:20:23 +0100

    Update grafos2.py

--- grafos2.py
+++ grafos2.py
@@ -1,55 +1,71 @@
 def newgraph(N):
     return [N,[]]
 
-
-
-def addedge(g,x,y):
+def edge_index(g,x,y):
     i=0
-    Found= False
+    f=0
+    notFound=True
+    notFound2=True
+    while (notFound or notFound2) and i<len(g[1]):
+        notFound= g[1][i]==(x,y)
+        notFound2= g[1][f]==(y,x)
+        f=f-1
+        i=i-1
+    return i
+
+def addegde(g,x,y):
+    i=0
+    notFound=True
     if x!=y:
-        while not Found and i<len(g[1]):
-            Found= (g[1][i]==(x,y))
+        while notFound and i<len(g[1]):
+            notFound= (g[1][i]==(x,y) and g[1][i]==(y,x))
             i=i+1
-        if Found:
+        if notFound:
             return g
         else:
             return [g[0],g[1]+[(x,y)]+[(y,x)]]
     else:
         return g
             
-def deledge(g,x,y):
+def delegde(g,x,y):
+    i=0
+    f=0
+    notFound=True
     if x!=y:
-        i=0
-        f=0
-        Found=False
-        Found2=False
-        while ((not Found)) and i<len(g[1]):
-            Found= (g[1][i]==(x,y))
-            i+=1
-        while ((not Found2)) and f<len(g[1]):
-            Found2 = g[1][f]==(y,x)   
+        while notFound and i<len(g[1]):
+            notFound= (g[1][i]==(x,y) and g[1][f]==(y,x))
+            i=i+1
             f=f+1
-        if not((Found) and (Found2)):
+        if notFound:
             return g
         else:
             if i<f:
-                g=[g[0],g[1][:i-1]+g[1][i:f-1]+g[1][f:]]
-                return g
+                return [g[0],g[1][:i-1]+g[1][i:f-1][f:]]
             else:
-                g=[g[0],g[1][:f-1]+g[1][f:i-1]+g[1][i:]]
-                return g
+                return [g[0],g[1][:f-1]+g[1][f:i-1]+g[1][i:]]
     else:
         return g
 
         
 
+def addegde(g,x,y):
+    i=edge_index(g,x,y)
+    if i==len(g[1]):
+        return [g[0],g[1]+[(x,y)]+[(y,x)]]
+    else:
+        return g
 
+def deledge(g,x,y):
+    i=edge_index(g,x,y)
+    return [g[0],g[1][:i-1]+g[1][i:]]
 
 def dim(g):
     return g[0]
 def emptyQ(e):
     return len(e[1])==0
 
+def edge(g,x,y):
+    return not len(g[1])==edge_index(g,x,y)
 
 def edge(g,x,y):
     i=0
@@ -67,7 +83,7 @@ def edge(g,x,y):
     else:
         return False
 
-def graphQ(e):
+def graphQ(e): #não funciona estou a corrigir
     if type(e)==list:
         if len(e)==2:
             if type(e[0])==int:
@@ -80,21 +96,19 @@ def graphQ(e):
                     if notfound:
                         if len(list_nodes(e))<=e[0]:
                             j=0
-                            while j<len(e[1]):
+                            while j<len(g[1]):
                                 k=0
                                 existe= True 
-                                while k<len(e[1]) and existe:
-                                    existe= e[1][j]==e[1][k]
-                                    k+=1
+                                while k<len(g[1]) and existe:
+                                    existe= g[1][j]==g[1][k]
                                 if existe:
                                     l=0
-                                    while l<len(e[1]):
-                                        l=0
-                                        diferente= diferente and (e[1][l][0]==e[1][l][1])
-                                        l+=1
+                                    while l<len(g[1]):
+                                        k=0
+                                        diferente= diferente and (g[1][k][0]==g[1][k][0])
                                     return diferente
                                 else:
-                                    return False    
+                                    return false    
                             else:
                                 return False
                     else: 
@@ -116,3 +130,4 @@ def list_nodes(g):
         if not e[1] in l:
             l+= [e[1]]
     return l
+

19896b6171d2b3365d974e46f24f3c089ad653cd
Martaaaaa1997 <148141613+Martaaaaa1997@users.noreply.github.com>
2023-10-26 15:25:47 +0100

    Update grafos2.py

--- grafos2.py
+++ grafos2.py
@@ -1,117 +1,122 @@
 def newgraph(N):
     return [N,[]]
 
-def edge_index(g,x,y):
-    i=0
-    f=0
-    notFound=True
-    notFound2=True
-    while (notFound or notFound2) and i<len(g[1]):
-        notFound= g[1][i]==(x,y)
-        notFound2= g[1][f]==(y,x)
-        f=f-1
-        i=i-1
-    return i
 
-def addegde(g,x,y):
+
+def addedge(g,x,y):
     i=0
-    notFound=True
+    Found= False
     if x!=y:
-        while notFound and i<len(g[1]):
-            notFound= (g[1][i]==(x,y) and g[1][i]==(y,x))
+        while not Found and i<len(g[1]):
+            Found= (g[1][i]==(x,y))
             i=i+1
-        if notFound:
+        if Found:
             return g
         else:
             return [g[0],g[1]+[(x,y)]+[(y,x)]]
     else:
         return g
             
-def delegde(g,x,y):
-    i=0
-    f=0
-    notFound=True
+def deledge(g,x,y):
     if x!=y:
-        while notFound and i<len(g[1]):
-            notFound= (g[1][i]==(x,y) and g[1][f]==(y,x))
-            i=i+1
+        i=0
+        f=0
+        Found=False
+        Found2=False
+        while ((not Found)) and i<len(g[1]):
+            Found= (g[1][i]==(x,y))
+            i+=1
+        while ((not Found2)) and f<len(g[1]):
+            Found2 = g[1][f]==(y,x)   
             f=f+1
-        if notFound:
+        if not((Found) and (Found2)):
             return g
         else:
             if i<f:
-                return [g[0],g[1][:i-1]+g[1][i:f-1][f:]]
+                g=[g[0],g[1][:i-1]+g[1][i:f-1]+g[1][f:]]
+                return g
             else:
-                return [g[0],g[1][:f-1]+g[1][f:i-1]+g[1][i:]]
+                g=[g[0],g[1][:f-1]+g[1][f:i-1]+g[1][i:]]
+                return g
     else:
         return g
 
         
 
-def addegde(g,x,y):
-    i=edge_index(g,x,y)
-    if i==len(g[1]):
-        return [g[0],g[1]+[(x,y)]+[(y,x)]]
-    else:
-        return g
 
-def deledge(g,x,y):
-    i=edge_index(g,x,y)
-    return [g[0],g[1][:i-1]+g[1][i:]]
 
 def dim(g):
     return g[0]
+
 def emptyQ(e):
     return len(e[1])==0
 
-def edge(g,x,y):
-    return not len(g[1])==edge_index(g,x,y)
 
 def edge(g,x,y):
     i=0
     f=0
-    notFound=True
+    Found=False
+    Found2= False
     if x!=y:
-        while notFound and i<len(g[1]):
-            notFound= (g[1][i]==(x,y) and g[1][f]==(y,x))
+        while not Found and i<len(g[1]):
+            Found= (g[1][i]==(x,y))
             i=i+1
+        while ((not Found2)) and f<len(g[1]):
+            Found2 = g[1][f]==(y,x)   
             f=f+1
-        if notFound:
-            return False
-        else:
+        if Found and Found2:
             return True
+        else:
+            return False
     else:
         return False
 
-def graphQ(e): #não funciona estou a corrigir
-    if type(e)==list:
-        if len(e)==2:
-            if type(e[0])==int:
-                if type(e[1])==list:
-                    i=0
-                    notfound=True
-                    while notfound and i<len(e[1]):
-                        notfound= len(e[1][i])==2
-                        i+=1
-                    if notfound:
-                        if len(list_nodes(e))<=e[0]:
-                            j=0
-                            while j<len(g[1]):
-                                k=0
-                                existe= True 
-                                while k<len(g[1]) and existe:
-                                    existe= g[1][j]==g[1][k]
-                                if existe:
-                                    l=0
-                                    while l<len(g[1]):
+def graphQ(e): # não funciona estou a corrigir
+        if type(e)==list:
+            if len(e)==2:
+                if type(e[0])==int:
+                    if type(e[1])==list:
+                        j=0
+                        intruso=False
+                        while j<len(e[1]) and not intruso:
+                            intruso= type(e[1][j])!=list
+                            print(intruso)
+                            j=j+1
+                        if not intruso:
+                            i=0
+                            notfound=True
+                            while notfound and i<len(e[1]):
+                                notfound= len(e[1][i])==2
+                                i+=1
+                            if notfound:
+                                if len(list_nodes(e))<=e[0]:
+                                    j=0
+                                    while j<len(e[1]):
                                         k=0
-                                        diferente= diferente and (g[1][k][0]==g[1][k][0])
-                                    return diferente
+                                        existe= True 
+                                        while k<len(e[1]) and existe:
+                                            existe= existe or (e[1][j][0]==e[1][k][1] or e[1][k][0]==e[1][j][1])
+                                            k+=1
+                                        j+=1
+
+                                        if existe:
+                                            l=0
+                                            diferente= True
+                                            while l<len(e[1]) and diferente:
+                                                diferente= (e[1][l][0]!=e[1][l][1])
+                                                l+=1
+                                            return diferente
+                                        else:
+                                            return False    
+                                    else:
+                                        return False
                                 else:
-                                    return false    
-                            else:
+                                    return False
+                            else: 
                                 return False
-                    else: 
+                        else:
+                            return False
+                    else:
                         return False
                 else:
                     return False
@@ -119,8 +124,7 @@ def graphQ(e): #não funciona estou a corrigir
                 return False
         else:
             return False
-    else:
-        return False
+
         
 def list_nodes(g):
     l=[]
@@ -130,4 +134,3 @@ def list_nodes(g):
         if not e[1] in l:
             l+= [e[1]]
     return l
-
